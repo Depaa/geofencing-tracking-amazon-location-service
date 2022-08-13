7e609a736d3317ca1f3fb83ef5363244231ec94b
Matteo Depascale <m.depascale@zero12.it>
2022-08-13 11:19:14 +0200

    feat(notification): add sms notification

--- src/lambdas/trigger-enter-event/index.js
+++ src/lambdas/trigger-enter-event/index.js
@@ -2,15 +2,24 @@ const SNS = require('aws-sdk/clients/sns');
 const sns = new SNS();
 
 exports.handler = async (event) => {
-  console.log(event);
+  console.debug(event);
 
   const params = {
-    Message: `Enjoy your ${event.detail.EventType} event`,
+    Message: JSON.stringify({
+      sms: `Enjoy your ${event.detail.EventType} event`,
+      email: `Enjoy your ${event.detail.EventType} event`,
+      default: `Enjoy your ${event.detail.EventType} event`,
+    }),
+    MessageAttributes: {
+      'AWS.SNS.SMS.SenderID': { DataType: 'String', StringValue: 'BlogTest' },
+      'AWS.SNS.SMS.SMSType': { DataType: 'String', StringValue: 'Transactional' },
+    },
     Subject: event.detail.EventType === 'ENTER' ? 'Welcome to my blog' : 'See you soon',
     TopicArn: `arn:aws:sns:${process.env.REGION}:${process.env.ACCOUNT_ID}:${process.env.TOPIC_NAME}`,
+    MessageStructure: 'json',
   };
 
   const response = await sns.publish(params).promise();
-  console.info(`Successfully sent message`);
-  console.info(response);
+  console.info(`Successfully sent message ${response.MessageId}`);
+  console.debug(response);
 };
