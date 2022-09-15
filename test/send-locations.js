d68bd17226ed71c9143f46ede8817cc22905fbe7
Depaa <depascale.matteo@gmail.com>
2022-09-15 09:39:27 +0200

    feat(test): update test script

--- test/send-locations.js
+++ test/send-locations.js
@@ -1,29 +1,26 @@
-const Location = require('aws-sdk/clients/location');
-const positions = require('./polygon.json');
+const { LocationClient, BatchUpdateDevicePositionCommand } = require('@aws-sdk/client-location');
+const { positionOut: P_OUT, positionIn: P_IN } = require('./polygon.json');
 
-const locationService = new Location({ region: 'eu-central-1' });
+const client = new LocationClient({ region: 'eu-central-1' });
 
-const COLLECTION_NAME = 'dev-geofencing-tracking-test-ls-geofence-collection';
-const POSITION_OUT = positions.positionOut;
-const POSITION_IN = positions.positionIn;
+const TRACKER_NAME = 'dev-geofencing-tracking-blog-ls-tracker';
 const COUNT_AFTER_CONSECUTIVE_OUT = 10;
 
 const handler = async () => {
   try {
     for (let index = 1; index <= 100; index++) {
-      await locationService
-        .batchEvaluateGeofences({
-          CollectionName: COLLECTION_NAME,
-          DevicePositionUpdates: [
-            {
-              DeviceId: 'test-device',
-              Position: index % COUNT_AFTER_CONSECUTIVE_OUT ? POSITION_OUT : POSITION_IN,
-              SampleTime: new Date(),
-            },
-          ],
-        })
-        .promise();
-      console.log(`#${index} - ${index % COUNT_AFTER_CONSECUTIVE_OUT ? 'POSITION_OUT' : 'POSITION_IN'}`);
+      const command = new BatchUpdateDevicePositionCommand({
+        TrackerName: TRACKER_NAME,
+        Updates: [
+          {
+            DeviceId: 'test-device',
+            Position: index % COUNT_AFTER_CONSECUTIVE_OUT ? P_OUT : P_IN,
+            SampleTime: new Date(),
+          }
+        ]
+      })
+      await client.send(command);
+      console.log(`#${index} - ${index % COUNT_AFTER_CONSECUTIVE_OUT ? 'P_OUT' : 'P_IN'}`);
 
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
